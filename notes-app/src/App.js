5e5a0dfcb032693e78163c176058ef08b87f23ab
abiton198 <abitonp@gmail.com>
2022-08-23 07:25:04 +0200

    .

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,43 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Editor from './Editor'
 import Sidebar from './Sidebar';
-import { data } from "./data"
+import { data } from "./data/data"
 import Split from "react-split"
 import {nanoid} from "nanoid" 
 
 export default function App() {
-  /**
-   * Challenge:
-   * 1. Every time the `notes` array changes, save it 
-   *    in localStorage. You'll need to use JSON.stringify()
-   *    to turn the array into a string to save in localStorage.
-   * 2. When the app first loads, initialize the notes state
-   *    with the notes saved in localStorage. You'll need to
-   *    use JSON.parse() to turn the stringified array back
-   *    into a real JS array.
-   */
-  
-  const [notes, setNotes] = React.useState([])
+  const [notes, setNotes] = React.useState(
+      () => JSON.parse(localStorage.getItem("notes")) || []
+  )
   const [currentNoteId, setCurrentNoteId] = React.useState(
       (notes[0] && notes[0].id) || ""
   )
+  
+  React.useEffect(() => {
+      localStorage.setItem("notes", JSON.stringify(notes))
+  }, [notes])
   
   function createNewNote() {
       const newNote = {
           id: nanoid(),
           body: "# Type your markdown note's title here"
       }
+      
       setNotes(prevNotes => [newNote, ...prevNotes])
       setCurrentNoteId(newNote.id)
   }
   
   function updateNote(text) {
-      setNotes(oldNotes => oldNotes.map(oldNote => {
-          return oldNote.id === currentNoteId
-              ? { ...oldNote, body: text }
-              : oldNote
-      }))
+      // Put the most recently-modified note at the top
+      setNotes(oldNotes => {
+          const newArray = []
+          for(let i = 0; i < oldNotes.length; i++) {
+              const oldNote = oldNotes[i]
+              if(oldNote.id === currentNoteId) {
+                  newArray.unshift({ ...oldNote, body: text })
+              } else {
+                  newArray.push(oldNote)
+              }
+          }
+          return newArray
+      })
+  }
+
+  function deleteNote(event, noteId) {
+      event.stopPropagation()
+      setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
   }
   
   function findCurrentNote() {
@@ -61,6 +70,7 @@ export default function App() {
                   currentNote={findCurrentNote()}
                   setCurrentNoteId={setCurrentNoteId}
                   newNote={createNewNote}
+                  deleteNote={deleteNote}
               />
               {
                   currentNoteId && 
